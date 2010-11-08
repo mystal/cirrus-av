7785b73d14c8691dfc0e016dd7bd2b38edd2bbbb
Gabriel Martinez <reitaka@gmail.com>
2010-11-08 15:48:13 -0500

    Added small file flagger program

---
+++ cirrus/Flagger.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class Flagger
+{
+    private Random rand;
+
+    public Flagger()
+    {
+        rand = new Random();
+    }
+
+    public boolean flag(String filename)
+    {
+        return rand.nextDouble() < 0.5;
+    }
+}
