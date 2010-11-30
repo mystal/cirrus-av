a05a1d3bed6e9010763ec0e0bb172e06fd6c2cc4
dmcfarl <doctordan81@gmail.com>
2010-11-30 16:36:16 -0500

    Added ability to download files from URLs with a -u option.  Server stores files in the output folder, and the Client stores files from URLs in the downloads folder.

---
+++ cirrus/Constants.java
@@ -0,0 +1,15 @@
+package cirrus;
+
+public class Constants {
+	public static final String FILE = "file";
+	public static final String URL = "url";
+	public static final String OUTPUT_FOLDER = "../output/";
+	public static final String DOWNLOADS_FOLDER = "../downloads/";	
+	
+	public static String getFileNameFromUrl(String url) {
+		if (url.indexOf("/") >= 0)
+			url = url.substring(url.lastIndexOf("/") + 1);
+		
+		return url;
+	}
+}
